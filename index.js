7a6fb54fcb0e5a4d98d955c9e7af816d5ee3addd
Paul-1783 <paulschmitt144@gmail.com>
2024-07-28 19:56:05 +0200

    made game  playable -  v1

--- index.js
+++ index.js
@@ -28,44 +28,61 @@ function getHumanChoice() {
 function playGame() {
   let humanScore = 0;
   let computerScore = 0;
+  let results = document.querySelector(".results");
+
   function playRound(hc, cc) {
     if (cc === hc) {
-      alert("Draw!");
+      results.textContent = "Draw!";
     } else if (cc === "scissors") {
       if (hc === "paper") {
-        alert(`You lose! ${cc} beats ${hc}`);
+        results.textContent = `You lose! ${cc} beats ${hc}`;
         computerScore++;
       } else {
-        alert(`You win! ${hc} beats ${cc}`);
+        results.textContent = `You win! ${hc} beats ${cc}`;
         humanScore++;
       }
     } else if (cc === "paper") {
       if (hc === "rock") {
-        alert(`You lose! ${cc} beats ${hc}`);
+        results.textContent = `You lose! ${cc} beats ${hc}`;
         computerScore++;
       } else {
-        alert(`You win! ${hc} beats ${cc}`);
+        results.textContent = `You win! ${hc} beats ${cc}`;
         humanScore++;
       }
     } else {
       if (hc === "scissors") {
-        alert(`You lose! ${cc} beats ${hc}`);
+        results.textContent = `You lose! ${cc} beats ${hc}`;
         computerScore++;
       } else {
-        alert(`You win! ${hc} beats ${cc}`);
+        results.textContent = `You win! ${hc} beats ${cc}`;
         humanScore++;
       }
     }
+    if (humanScore === 5 || computerScore === 5) {
+      results.textContent = `Final Score: Computer ${computerScore}, Human ${humanScore}`;
+      humanScore = 0;
+      computerScore = 0;
+    } else
+      results.textContent += `\n\r Score: Computer ${computerScore}, Human ${humanScore}`;
   }
 
-  let i = 0;
-  while (i < 5) {
-    let humanSelection = getHumanChoice();
+  let buttons = document.querySelector("#buttons");
+  buttons.addEventListener("click", (event) => {
+    let target = event.target;
     let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-    ++i;
-  }
-  alert(`Final Score: Computer ${computerScore}, Human ${humanScore}`);
+
+    switch (target.id) {
+      case "paper":
+        playRound("paper", computerSelection);
+        break;
+      case "scissors":
+        playRound("scissors", computerSelection);
+        break;
+      case "rock":
+        playRound("rock", computerSelection);
+        break;
+    }
+  });
 }
 
 playGame();
